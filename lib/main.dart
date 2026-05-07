74cb4a4586831bd9d8f7e7621145aac956100a90
G Bhupathe <gbupathie2006@gmail.com>
2026-05-07 19:07:32 +0530

    sign in

--- lib/main.dart
+++ lib/main.dart
@@ -8,163 +8,126 @@ class Assigments extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Assignment2(),
+      home: Assignment3(),
       //color: Colors.deepPurple.shade400,
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class Assignment1 extends StatelessWidget{
+class Assignment3 extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    var doRemember = false;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Assignment 1"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple.shade200,
-      ),
-      body: Center(
-        child: Container(
-          width: 600,
-          height: 600,
-          color: Colors.red.shade400,
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 60),
-                Container(
-                  width: 480,
-                  height: 210,
-                  color: Colors.yellow.shade300,
-                  child: Center(
-                    child: Row(
-                      children: [
-                        SizedBox(width: 61,),
-                        Container(
-                          height: 120,
-                          width: 146.5,
-                          color: Colors.cyan.shade300,
-                          child: Center(
-                            child: Text("Top-Left",
-                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 61,),
-                        Container(
-                          height: 120,
-                          width: 146.5,
-                          color: Colors.cyan.shade300,
-                          child: Center(
-                            child: Text("Top-Right",
-                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 61),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 60),
-                Container(
-                  width: 480,
-                  height: 210,
-                  color: Colors.yellow.shade300,
-                    child: Center(
-                        child: Row(
-                          children: [
-                            SizedBox(width: 61,),
-                            Container(
-                              height: 120,
-                              width: 146.5,
-                              color: Colors.cyan.shade300,
-                              child: Center(
-                                child: Text("Bottom-Left",
-                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: 61,),
-                            Container(
-                              height: 120,
-                              width: 146.5,
-                              color: Colors.cyan.shade300,
-                              child: Center(
-                                child: Text("Bottom-Right",
-                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: 61),
-                          ],
-                        ),
-                    ),
-                ),
-                SizedBox(height: 60),
-              ],
-            ),
-          ),
-        ),
-      ),
-      backgroundColor: Colors.lightGreen.shade200,
-    );
-  }
-}
-
-class Assignment2 extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children:[
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Row(
+          children: [
             Container(
-              width: 320,
-              height: 400,
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: double.infinity,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage("assets/assignment2.jpg"),
-                  fit: BoxFit.cover,
+                    image: AssetImage('assets/assignment3.jpg'),
+                    fit: BoxFit.cover,
                 ),
-                borderRadius: BorderRadius.circular(30)
               ),
             ),
-            //SizedBox(height: 30),
-            Column(
-              children: [
-                Text("Tang San",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20,
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text("This anime is called Soul Land",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ],
-            ),
+            SizedBox(width: 100,),
             Container(
-              width: 48,
-              height: 48,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent.shade200,
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.arrow_forward,
-                color: Colors.white,
-                size: 24,
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Sign In",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 50,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      SizedBox(height: 20,),
+                      Column(
+                        children: [
+                          Container(
+                            width: 300,
+                            child: TextField(
+                                decoration: InputDecoration(
+                                  labelText: "Username",
+                                  border: OutlineInputBorder(),
+                                  prefixIcon: Icon(Icons.person),
+                                )
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            width: 300,
+                            child: TextField(
+                                decoration: InputDecoration(
+                                  labelText: "Password",
+                                  border: OutlineInputBorder(),
+                                  prefixIcon: Icon(Icons.lock),
+                                )
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Container(
+                        width: 300,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.check_box_outline_blank),
+                                SizedBox(width: 5),
+                                Text("Remember Me"),
+                              ],
+                            ),
+                            Text("Forgot Password?",
+                              style: TextStyle(
+                                color: Colors.purpleAccent,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      ElevatedButton(
+                          onPressed: (){
+                            print("Signed in Successfully");
+                          },
+                          child: Text("Sign in",
+                            style: TextStyle(
+                              color: Colors.purpleAccent,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            )
+                          ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("New Here?"),
+                          SizedBox(width: 5),
+                          Text("Create an Account",
+                            style: TextStyle(
+                              color: Colors.purpleAccent,
+                            )
+                          )
+                        ]
+                      )
+                    ]
+                      ),
               ),
             ),
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
